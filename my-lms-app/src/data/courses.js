4c65339f37c713aa74b3c73b357e809928e63135
8bstn <sebsilva10@hotmail.com>
2025-03-30 23:18:40 -0600

    Update courses.js

--- my-lms-app/src/data/courses.js
+++ my-lms-app/src/data/courses.js
@@ -5,7 +5,7 @@ const courses = [
       instructor: "Dr. John Smith",
       description: "Master HTML, CSS, and JavaScript.",
       duration: "8 weeks",
-      image: "images/course1.jpg"
+      image: "./images/course1.jpg"
     },
     {
       id: 2,
@@ -13,7 +13,7 @@ const courses = [
       instructor: "Dr. Emily Carter",
       description: "Understand and implement efficient data structures.",
       duration: "10 weeks",
-      image: "images/course2.jpg"
+      image: "./images/course2.jpg"
     },
     {
       id: 3,
@@ -21,7 +21,7 @@ const courses = [
       instructor: "Dr. Sarah Lee",
       description: "Learn SQL, normalization, and database design.",
       duration: "9 weeks",
-      image: "images/course3.jpg"
+      image: "./images/course3.jpg"
     },
     {
       id: 4,
@@ -29,7 +29,7 @@ const courses = [
       instructor: "Prof. Michael Tan",
       description: "Test software using manual and automated techniques.",
       duration: "7 weeks",
-      image: "images/course4.jpg"
+      image: "./images/course4.jpg"
     },
     {
       id: 5,
@@ -37,7 +37,7 @@ const courses = [
       instructor: "Dr. Lisa Walker",
       description: "Explore architectural styles and design patterns.",
       duration: "8 weeks",
-      image: "images/course5.jpg"
+      image: "./images/course5.jpg"
     },
     {
       id: 6,
@@ -45,7 +45,7 @@ const courses = [
       instructor: "Dr. Kevin Patel",
       description: "Analyze and visualize data using Python.",
       duration: "10 weeks",
-      image: "images/course6.jpg"
+      image: "./images/course6.jpg"
     },
     {
       id: 7,
@@ -53,7 +53,7 @@ const courses = [
       instructor: "Dr. Angela Park",
       description: "Learn supervised and unsupervised ML techniques.",
       duration: "12 weeks",
-      image: "images/course7.jpg"
+      image: "./images/course7.jpg"
     },
     {
         id: 8,
@@ -61,7 +61,7 @@ const courses = [
         instructor: "Dr. Ethan Brooks",
         description: "Learn how to protect systems from security threats.",
         duration: "6 weeks",
-        image: "images/course8.jpg"
+        image: "./images/course8.jpg"
     },
     {
         id: 9,
@@ -69,7 +69,7 @@ const courses = [
         instructor: "Prof. Natalie Cruz",
         description: "Dive into AWS, Azure, and cloud infrastructure basics.",
         duration: "9 weeks",
-        image: "images/course9.jpg"
+        image: "./images/course9.jpg"
     },
     {
         id: 10,
@@ -77,7 +77,7 @@ const courses = [
         instructor: "Dr. Omar Khan",
         description: "Build responsive mobile apps using Flutter and React Native.",
         duration: "10 weeks",
-        image: "images/course10.jpg"
+        image: "./images/course10.jpg"
     }
       
   ];
